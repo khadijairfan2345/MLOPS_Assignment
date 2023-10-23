445db3fcf0de04c048d2e6ae669e03dfe56c0011
khadijairfan2345 <77497960+khadijairfan2345@users.noreply.github.com>
2023-10-23 21:01:19 +0500

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -4,16 +4,15 @@ from joblib import load
 
 class TestModel(unittest.TestCase):
 
+
     def test_train_model(self):
         train_model()
         clf = load("model.pkl")
-        # Test the trained model with a sample data point
-        sample_data = [13.05, 2.05, 3.22, 25.0, 
-                       124, 2.63, 2.68, 0.47, 
-                       1.92, 3.58, 1.13, 3.2, 830]
+        sample_data = [13.05, 2.05, 3.22, 25.0, 124, 2.63, 
+                       2.68, 0.47, 1.92, 3.58, 1.13, 3.2, 830]
         prediction = clf.predict([sample_data])
-        self.assertIn(prediction[0], [0, 1, 2]) 
-        # Since wine dataset has 3 classes
+        self.assertIn(prediction[0], [0, 1, 2])  # Since wine dataset has 3 classes
+
 
 if __name__ == "__main__":
     unittest.main()
